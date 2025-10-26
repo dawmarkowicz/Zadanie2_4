62d52854085d1debb698da9b985c2aea34f1f9a1
dawmarkowicz <daw.markowicz123@gmail.com>
2025-10-26 08:38:12 +0000

    zmiana napisu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,11 @@
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("skopiowane!");
   }
 
   // @Test
   // void addition() {
-  //     assertEquals(2, 1 + 1);
+  // assertEquals(2, 1 + 1);
   // }
 }
