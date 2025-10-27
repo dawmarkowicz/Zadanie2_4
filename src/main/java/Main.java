435173bda425b4c1d5b13f72f92248aa782ee0e3
dawmarkowicz <daw.markowicz123@gmail.com>
2025-10-27 08:10:05 +0000

    zadanie2_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,42 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj liczbe: ");
-        int a = scanner.nextInt();
-        System.out.println("podaj druga liczbe: ");
-        int b = scanner.nextInt();
-        System.out.println("suma: " + (a + b));
+        System.out.println("1 - dodawanie");
+        System.out.println("2 - odejmowanie");
+        System.out.println("3 - mnożenie");
+        System.out.println("4 - dzielenie");
 
+        System.out.println("Podaj numer dzialania");
+        int dzialanie = scanner.nextInt();
+        System.out.println("Podaj pierwsza liczbe");
+        float liczba1 = scanner.nextFloat();
+        System.out.println("Podaj druga liczbe");
+        float liczba2 = scanner.nextFloat();
+
+        switch (dzialanie) {
+            case 1:
+                System.out.println("Wynik dodawania:");
+                System.out.println(liczba1 + liczba2);
+                break;
+            case 2:
+                System.out.println("Wynik odejmowania:");
+                System.out.println(liczba1 - liczba2);
+                break;
+            case 3:
+                System.out.println("Wynik mnozenia:");
+                System.out.println(liczba1 * liczba2);
+                break;
+            case 4:
+                // zabezpieczenie przed dzieleniem przez zero
+                if (liczba2 == 0) {
+                    System.out.println("Nie dzielimy przez zero!");
+                    break;
+                } else {
+                    System.out.println("Wynik dzielenia:");
+                    System.out.println(liczba1 / liczba2);
+                    break;
+                }
+        }
         scanner.close();
     }
 }
